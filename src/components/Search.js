4d7ff8265975103b3418019bebe3f43382e3b214
Danish Agarwal <danishagarwal9@gmail.com>
2023-07-16 19:19:45 +0530

    other_Updates

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,33 +1,47 @@
 import React, { useState } from "react";
 
 const Search = () => {
-  const [state, setState] = useState("");
+  const [text, setText] = useState(""); //Storing the searched value
+  const [weather, setWeather] = useState(""); //Storing the fetched weather Data
+
   const api = {
     base: "https://api.openweathermap.org/data/2.5/",
     key: "4f8e795dcd6dbf7b9f5276bff095ffc1",
   };
-  const [text, setText] = useState("");
 
-  async function handleClick() {
+  async function handleClick(e) {
     const data = await fetch(
       `${api.base}weather?q=${text}&appid=${api.key}&units=metric`
     );
     const json = await data.json();
-    console.log(json);
+    if (!json) {
+      return;
+    } else {
+      (setWeather) => setWeather(json);
+      console.log(weather);
+      setText("");
+    }
   }
+  console.log(text);
 
   return (
     <>
       <div>
         <h2> React Weather Application </h2>
-        <div>
+        <div className="search">
           <h3> Search City</h3>
+
           <input
             onChange={(e) => setText(e.target.value)}
             value={text}
             type="text"
           ></input>
           <button onClick={handleClick}>Search</button>
+        </div>
+
+        <div className="weather">
+          <h1>{weather.name}</h1>
+          <h2>{weather.message}</h2> {/* if there is a wrong input  */}
         </div>
       </div>
     </>
