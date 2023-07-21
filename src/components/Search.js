33df82a7f93bec7805e8c9a46bbcaaefb00b6095
Danish Agarwal <danishagarwal9@gmail.com>
2023-07-21 11:11:31 +0530

    Change Background image code

--- src/components/Search.js
+++ src/components/Search.js
@@ -34,9 +34,31 @@ const Search = () => {
     </div>
   );
 
+  const getBackgroundImage = () => {
+    if (weather && weather.weather[0].main === "Clouds") {
+      return "url('https://media.istockphoto.com/id/612023744/photo/blue-and-orange-sunset-sky.jpg?s=612x612&w=0&k=20&c=bqPHaI4d3ghIbLIwtbTDPxV8QQ2pIN2mbCfzLb7gucQ=')"; // Replace with the URL of the hot background image
+    } else if (weather && weather.weather[0].main === "Rain") {
+      return "url('https://media.istockphoto.com/id/1049365996/photo/rain-fall-on-the-ground.jpg?s=612x612&w=0&k=20&c=lH73ofHt2WKtJhatw8H53DN4EfgV4ZKDEGPqO4xOPho=')"; // Replace with the URL of the cold background image
+    } else if (weather && weather.weather[0].main === "Clear") {
+      return "url('https://img.freepik.com/premium-photo/beautiful-airatmosphere-bright-blue-sky-background-abstract-clear-texture-with-white-clouds_29332-3832.jpg')";
+    } else {
+      return "url('default-background-image.jpg')"; // Replace with the URL of the default background image
+    }
+  };
+
+  // Define the inline style for the background image
+  const backgroundStyle = {
+    backgroundImage: getBackgroundImage(),
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    minHeight: "100vh", // Set the minimum height to cover the entire viewport
+    minWidth: "100vh",
+  };
+
   return (
     <>
-      <div className="container">
+      <div className="container" style={backgroundStyle}>
         <h2> React Weather Application </h2>
         <h3> Search City</h3>
         <div className="weather">
